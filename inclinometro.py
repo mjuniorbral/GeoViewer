f99f6a8c35cb8ff17be866233df3f21cddb335be
Marcelo Cabral dos Santos Junior <mjuniorbral@gmail.com>
2024-09-10 18:25:08 -0300

    Inclinometro Beta
    Primeira renderização de gráfico do inclinômetro considerando modelo feito para o INC02 (atualização do modelo de dados no excel).
    
    Remoção do arquivo modelo do excel do .gitignore para inclusão do modelo no repositório

---
+++ inclinometro.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+from modules.functions import readSheets
+from modules.classes import *
+import matplotlib.pyplot as plt
+
+planilhas = readSheets("INC02-POR-2024-08.xlsx")
+nomePlanilhaTeste = "27-08-2024"
+df = planilhas[nomePlanilhaTeste].copy(deep=True)
+df.columns = df.iloc[2]
+df = df.drop(index=[0,1,2])
+X = df["desloc_B"]
+Y = df["Depth"]
+series = [Serie(X,Y,label=nomePlanilhaTeste,setup=dict(
+    marker="o"
+))]
+
+# Criar um setup padrão de inclinômetro baseado nesse setup, e implementar novas alterações
+graph = Grafico(series,
+                title="Desloc. Eixo - INC02",
+                setup=dict(
+                    width = 5,
+                    height = 12,
+                    xMajorFormatter = FuncFormatter(getFunctionToFuncFormatter(4)),
+                    # xMinorFormatter = "",
+                    yMajorFormatter = FuncFormatter(getFunctionToFuncFormatter(2)),
+                    # yMinorFormatter = "",
+                    y2MajorFormatter = FuncFormatter(getFunctionToFuncFormatter(2)),
+                    # y2MinorFormatter = "",
+                    xMajorLocator = AutoLocator(),
+                    xMinorLocator = AutoMinorLocator(1),
+                    yMinorLocator = AutoMinorLocator(),
+                    yMajorLocator = AutoLocator(),
+                    ylim = (-31,0),
+                    xlim = (-0.002,0.002),
+                    yLabelFontsize = 10,
+                    yLabel = "Profundidade (m)",
+                    xLabelFontsize = 10,
+                    xLabel = "Desloc. Horizontal (m)",
+))
+graph.render()
+graph.save(path="inc002.png")
