6a4012693fe612510e67e6009736a21d09d55511
Marcelo Cabral dos Santos Junior <mjuniorbral@gmail.com>
2024-09-11 14:11:49 -0300

    moving separador to functions

--- modules/functions.py
+++ modules/functions.py
@@ -256,6 +256,27 @@ def gerarHistorico(nomeInstrumento,
             historico.loc[:,f"{nomeInstrumento} ({string})"] = get_info_instrument(nomeInstrumento,column)
     return historico
 
-
-
-
+def separador(path_csv,sep=","):
+    out_lines = []
+    try:
+        with open(path_csv) as arquivo:
+            for line in arquivo.readlines():
+                if line[0]=="-":
+                    newLine = []
+                    cont = 0
+                    for c in line:
+                        if c == sep:
+                            cont+=1
+                            if cont%2==0:
+                                newLine.append(";")
+                            else:
+                                newLine.append(c)
+                            continue
+                        newLine.append(c)
+                    out_lines.append("".join(newLine),end="")
+                else:
+                    out_lines.append(line.replace(",",";"),end="")
+                    
+    finally:
+        arquivo.close()
+    return "\n".join(out_lines)
