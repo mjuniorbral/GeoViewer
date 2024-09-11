45da8df61c3dddd1063345dd7905aef729fa5ca3
Marcelo Cabral dos Santos Junior <mjuniorbral@gmail.com>
2024-09-11 13:59:22 -0300

    Update separador.py
    Transformação da operação em subrotina na função separador

--- separador.py
+++ separador.py
@@ -1,22 +1,24 @@
-sep = ","
-try:
-    with open("AGLBRPINC02(7).csv") as arquivo:
-        for line in arquivo.readlines():
-            if line[0]=="-":
-                newLine = []
-                cont = 0
-                for c in line:
-                    if c == sep:
-                        cont+=1
-                        if cont%2==0:
-                            newLine.append(";")
-                        else:
-                            newLine.append(c)
-                        continue
-                    newLine.append(c)
-                print("".join(newLine),end="")
-            else:
-                print(line.replace(",",";"),end="")
-                
-finally:
-    arquivo.close()
+def separador(path_csv,sep=","):
+    out_lines = []
+    try:
+        with open(path_csv) as arquivo:
+            for line in arquivo.readlines():
+                if line[0]=="-":
+                    newLine = []
+                    cont = 0
+                    for c in line:
+                        if c == sep:
+                            cont+=1
+                            if cont%2==0:
+                                newLine.append(";")
+                            else:
+                                newLine.append(c)
+                            continue
+                        newLine.append(c)
+                    out_lines.append("".join(newLine),end="")
+                else:
+                    out_lines.append(line.replace(",",";"),end="")
+                    
+    finally:
+        arquivo.close()
+    return "\n".join(out_lines)
